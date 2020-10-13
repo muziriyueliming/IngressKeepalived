a24d1641fed2293eb4fcae7e76aeffb989d449b9
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-13 15:38:34 +0800

    同步推送

--- dingding.py
+++ dingding.py
@@ -2,10 +2,47 @@
 # coding:utf-8
 # zabbix钉钉报警
 import hashlib, base64, urllib, hmac, requests, json, sys, os, datetime, time
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import sys
 
 dingding_token = os.getenv('ROBOT_TOKEN')
 dingding_secret = os.getenv('SECRET')
 dingding_text = sys.argv[1]
+
+LOG_DIR = "/tmp"
+LOG_FILE = "dingding.log"
+LOG_LEVEL = "INFO"
+
+log_level = getattr(logging, LOG_LEVEL)
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR, 750)
+RecodeLog = logging.getLogger("LOG INFO")
+RecodeLog.setLevel(log_level)
+# 建立一个filehandler来把日志记录在文件里，级别为debug以上
+# 按天分割日志,保留30天
+if not RecodeLog.handlers:
+    fh = TimedRotatingFileHandler(
+        os.path.join(LOG_DIR, LOG_FILE), when='D', interval=1, backupCount=30
+    )
+    ch = logging.StreamHandler()
+    fh.setLevel(log_level)
+    ch.setLevel(log_level)
+    # 设置日志格式
+    if LOG_LEVEL == "DEBUG":
+        formatter = logging.Formatter(
+            "%(asctime)s - Message of File(文件): %(filename)s,Module(类):%(module)s,FuncName(函数):%(funcName)s ,LineNo(行数):%(lineno)d - %(levelname)s - %(message)s"
+        )
+    else:
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    # 将相应的handler添加在logger对象中
+    RecodeLog.addHandler(fh)
+    RecodeLog.addHandler(ch)
 
 
 def send_alert(token, content, secret):
@@ -38,8 +75,9 @@ def send_alert(token, content, secret):
         x = requests.post(url=url, data=json.dumps(data), headers=headers)
         if x.json()["errcode"] != 0:
             raise Exception(x.content)
+        RecodeLog.info(msg="发送报警成功,url:{0},报警内容:{1}".format(url, data))
     except Exception as error:
-        print(error)
+        RecodeLog.info(msg="发送报警失败,url:{0},报警内容:{1},原因:{2}".format(url, data, error))
 
 
 if not dingding_token or not dingding_secret:
--- ingress_health_check.py
+++ ingress_health_check.py
@@ -1,7 +1,43 @@
 # -*- coding:utf-8 -*-
 import requests
 from requests import HTTPError, RequestException
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
 import sys
+
+LOG_DIR = "/tmp"
+LOG_FILE = "ingress_health_check.log"
+LOG_LEVEL = "INFO"
+
+log_level = getattr(logging, LOG_LEVEL)
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR, 750)
+RecodeLog = logging.getLogger("LOG INFO")
+RecodeLog.setLevel(log_level)
+# 建立一个filehandler来把日志记录在文件里，级别为debug以上
+# 按天分割日志,保留30天
+if not RecodeLog.handlers:
+    fh = TimedRotatingFileHandler(
+        os.path.join(LOG_DIR, LOG_FILE), when='D', interval=1, backupCount=30
+    )
+    ch = logging.StreamHandler()
+    fh.setLevel(log_level)
+    ch.setLevel(log_level)
+    # 设置日志格式
+    if LOG_LEVEL == "DEBUG":
+        formatter = logging.Formatter(
+            "%(asctime)s - Message of File(文件): %(filename)s,Module(类):%(module)s,FuncName(函数):%(funcName)s ,LineNo(行数):%(lineno)d - %(levelname)s - %(message)s"
+        )
+    else:
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    # 将相应的handler添加在logger对象中
+    RecodeLog.addHandler(fh)
+    RecodeLog.addHandler(ch)
 
 
 def health(url, method):
@@ -16,11 +52,12 @@ def health(url, method):
     try:
         res = requests.request(method=method, url=url, timeout=3)
         if res.status_code != 200:
-            raise HTTPError("连接状态异常!状态:{0},报错信息:{1}".format(res.status_code, res.reason))
+            raise HTTPError(res.content)
+        RecodeLog.info(msg="发送报警成功,url:{0},返回内容:{1}".format(url, res.content))
         sys.exit(0)
     except RequestException as error:
-        print(error)
+        RecodeLog.error(msg="发送报警失败,url:{0},返回内容:{1}".format(url, error))
         sys.exit(1)
 
 
-health(url="http://10.253.100.12:80", method="post")
+health(url="http://10.253.100.12:31110/actuator/health", method="get")
